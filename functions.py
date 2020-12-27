2fd88d317cfaeab5e37a8d76595e5bd0c81969fb
theonlyanil <theonlyanil@gmail.com>
2020-12-27 16:34:42 +0530

    massive change to column and columnCell formatting.

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from utils import formatColumn, chunk_list, epochToDatetimeList
+from utils import formatColumns, chunk_list, epochToDatetimeList
 from engines import v8_period, v8_range, v7multi, v10
 
 """ Gets a company's asset profile as in: address, summary, website, employees, etc. """
@@ -96,34 +96,13 @@ def getIncomeStatementHistory(symbol):
     df = pd.DataFrame(income_statements)
 
     # Format cells
-    df['endDate'] = formatColumn(df['endDate'], 'fmt')
-    df['totalRevenue'] = formatColumn(df['totalRevenue'], 'raw')
-    df['costOfRevenue'] = formatColumn(df['costOfRevenue'], 'raw')
-    df['grossProfit'] = formatColumn(df['grossProfit'], 'raw')
-    df['researchDevelopment'] = formatColumn(df['researchDevelopment'], 'raw')
-    df['sellingGeneralAdministrative'] = formatColumn(df['sellingGeneralAdministrative'], 'raw')
-    df['nonRecurring'] = formatColumn(df['nonRecurring'], 'raw')
-    df['otherOperatingExpenses'] = formatColumn(df['otherOperatingExpenses'], 'raw')
-    df['operatingIncome'] = formatColumn(df['operatingIncome'], 'raw')
-    df['totalOperatingExpenses'] = formatColumn(df['totalOperatingExpenses'], 'raw')
-    df['totalOtherIncomeExpenseNet'] = formatColumn(df['totalOtherIncomeExpenseNet'], 'raw')
-    df['ebit'] = formatColumn(df['ebit'], 'raw')
-    df['interestExpense'] = formatColumn(df['interestExpense'], 'raw')
-    df['incomeBeforeTax'] = formatColumn(df['incomeBeforeTax'], 'raw')
-    df['incomeTaxExpense'] = formatColumn(df['incomeTaxExpense'], 'raw')
-    df['minorityInterest'] = formatColumn(df['minorityInterest'], 'raw')
-    df['netIncomeFromContinuingOps'] = formatColumn(df['netIncomeFromContinuingOps'], 'raw')
-    df['discontinuedOperations'] = formatColumn(df['discontinuedOperations'], 'raw')
-    df['extraordinaryItems'] = formatColumn(df['extraordinaryItems'], 'raw')
-    df['effectOfAccountingCharges'] = formatColumn(df['effectOfAccountingCharges'], 'raw')
-    df['otherItems'] = formatColumn(df['otherItems'], 'raw')
-    df['netIncome'] = formatColumn(df['netIncome'], 'raw')
-    df['netIncomeApplicableToCommonShares'] = formatColumn(df['netIncomeApplicableToCommonShares'], 'raw')
-
+    df = formatColumns(df)
     return df
 
 def getIncomeStatementsQtr(symbol):
     income_statements = v10(symbol, 'incomeStatementHistoryQuarterly')['incomeStatementHistoryQuarterly']['incomeStatementHistory']
     df = pd.DataFrame(income_statements)
+
+    df = formatColumns(df)
     df.to_csv('okok.csv')
     return df
--- utils.py
+++ utils.py
@@ -1,22 +1,32 @@
 from datetime import datetime
 
 """
-    Column Formatting based on 'raw' or 'fmt'.
+    Column Cell Formatting based on 'raw' or 'fmt'.
     It takes in a dataframe series and formats it based on input dataType.
     Before formatting, cell's data has a dict of data e.g.:
         {'raw': 12321, 'fmt':'12,321'}
     Input: formatCell(columnData, 'raw')
     Output: 12321
 """
-def formatColumn(df_series, dataType):
+def formatColumnCells(df_series, dataType):
     results = []
     for item in df_series:
         try:
             item = item[dataType]
             results.append(item)
         except Exception as e:
-            results.append(0)
+            results.append(item)
     return results
+
+def formatColumns(df):
+    # Column Formatting
+    for col in df:
+        if col == 'endDate':
+            df[col] = formatColumnCells(df[col], 'fmt')
+        else:
+            df[col] = formatColumnCells(df[col], 'raw')
+
+    return df
 
 """
     Breaks a list into a chunk of pre defined size.
